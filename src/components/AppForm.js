5c1f8c169ac3634a39c357cb3824f0d598fe8793
RoxMayra <mayrapuma2023@gmail.com>
2023-10-24 19:41:22 -0500

    se conf. registro

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from './Firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 const AppForm = () => {
-    return (
-        <div style={{background:"orange",padding:"10px",textAlign:"center"}}>
-            <h1>AppForm.js</h1>
-            <p>Nombre</p>
-            <p>Edad</p>
-            <p>Genero</p>
-            <p>Guardar</p>
-        </div>
-    )
+    ////GUARDAR / ACTUALIZAR ////
+    const camposRegistro = {nombre:"",edad:"",genero:""}
+    const [objeto, setObjeto] = useState(camposRegistro);
+
+  const handleSubmit = (e) =>{ 
+    e.preventDefault();
+    try {
+      if(db){
+        addDoc(collection(db,'persona'), objeto);
+        console.log("Se guardo con exito");
+      }
+
+  } catch (error) {
+    console.error();
+  }
+
+  }
+  return (
+    <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
+    <form onSubmit={handleSubmit}>
+      <h1>AppForm.js</h1>
+      <input type='texto' placeholder='Nombre.....'/>
+      <input type='texto' placeholder='Nombre.....'/>
+      <select>
+      <option value="">Seleccione</option>
+      <option value="M">Masculino</option>
+      <option value="F">Femenino</option>
+      </select>
+      <button>Guardar/Actualizar</button>
+    </form>
+    </div>
+  )
 }
 
 export default AppForm
+
